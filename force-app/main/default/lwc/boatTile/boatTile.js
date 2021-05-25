4016871ce6a79aa298b30f4a32bc29be12481401
unknown <razvan.ioans96@gmail.com>
2021-05-25 15:02:54 +0200

    Boat tile development

--- force-app/main/default/lwc/boatTile/boatTile.js
+++ force-app/main/default/lwc/boatTile/boatTile.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 // imports
 
@@ -7,17 +7,31 @@ const TILE_WRAPPER_UNSELECTED_CLASS = 'tile-wrapper';
 
 export default class BoatTile extends LightningElement {
     
-    boat;
-    selectedBoatId;
+    @api boat;
+    @api selectedBoatId;
     
     // Getter for dynamically setting the background image for the picture
-    get backgroundStyle() { }
+    get backgroundStyle() { 
+      return "background-image:url(${this.boat.Picture__c})";
+    }
     
     // Getter for dynamically setting the tile class based on whether the
     // current boat is selected
-    get tileClass() { }
-
+    get tileClass() { 
+      if(this.selectedBoatId){
+        return TILE_WRAPPER_SELECTED_CLASS;
+      }
+      return TILE_WRAPPER_UNSELECTED_CLASS;
+    }
+  
     // Fires event with the Id of the boat that has been selected.
-    selectBoat() { }
+    selectBoat() {
+      this.selectedBoatId = this.boat.Id
+      this.dispatchEvent(new CustomEvent('boatselect', {
+        detail: {
+          boatId : this.selectedBoatId
+        }
+      }))
+    }
   }
   
